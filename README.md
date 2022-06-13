9a0e905780a8b7479bd5f945d3a9c9ee1887ee88
mikeyhodl <mike.owino40@yahoo.com>
2022-06-13 16:11:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ashhy9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ashhy9)
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import './style.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
